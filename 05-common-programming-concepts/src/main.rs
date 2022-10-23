01c54cbd3e007c280fab087b7cce7d25f6ca358a
Zach Broniszewski <zachbroniszewski@gmail.com>
2022-10-23 16:39:23 -0400

    feat(05-common-programming-concepts): add examples (numeric operations, tuples, arrays)

--- 05-common-programming-concepts/src/main.rs
+++ 05-common-programming-concepts/src/main.rs
@@ -1,8 +1,13 @@
+use std::f64;
+
 fn main() {
     immutable_example();
     constants_example();
     shadowing_example();
     integer_overflow_example();
+    numeric_operations_example();
+    tuples_example();
+    arrays_example();
 }
 
 fn immutable_example() {
@@ -33,4 +38,41 @@ fn integer_overflow_example() {
     
     let x: u8 = 255;
     println!("value of x: {}", x);
+}
+
+fn numeric_operations_example() {
+  // addition
+  let _sum = 5 + 10;
+
+  // subtraction
+  let _difference = 95.5 - 4.3;
+
+  // multiplication
+  let _product = 4 * 30;
+
+  // division
+  let _quotient = 56.7 / 32.2;
+  // results in 0
+  let _floored = 2 / 3;
+
+  // remainder
+  let _remainder = 43 % 5;
+}
+
+fn tuples_example() {
+  let tup: (char, i32, f64) = ('A', 45, 45.0);
+
+  // destructuring
+  let (a, b, _) = tup;
+  // index access
+  let c = tup.2;
+
+  println!("{a}, {b}, {:.1}", c);
+}
+
+fn arrays_example() {
+  // initialize with different values
+  let _arr: [u8; 3] = [1, 2, 3];
+  // initialize with same values [8, 8, 8]
+  let _arr_two = [8; 3];
 }
